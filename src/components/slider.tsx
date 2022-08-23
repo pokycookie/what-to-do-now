4c6d3cf8330d09a87817a97f1763bc0b2642f7d6
pokycookie <0cookieboy0@gmail.com>
2022-08-23 21:00:33 +0900

    slider

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,41 +1,61 @@
-import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import "../scss/components/slider.scss";
 
 interface IProps {
   min: number;
   max: number;
-  step?: number;
   default?: number;
+  height?: number;
+  count?: number;
   onChange?: (value: number) => void;
 }
 
+interface IStyles {
+  top?: string;
+  opacity?: number;
+  transform?: string;
+  height?: string;
+}
+
 export default function Slider(props: IProps) {
-  const step = props.step || 1;
+  // default값이 min ~ max 범위에 있는지 확인 필요
+  const defaultIndex = props.default ? props.default - props.min : 0;
+  const HEIGHT = props.height || 25;
+  const COUNT = props.count || 5;
 
-  const [value, setValue] = useState(props.default || 0);
-  const [prev, setPrev] = useState(value - step < props.min ? props.max : value - step);
-  const [next, setNext] = useState(value + step > props.max ? props.min : value + step);
+  const [valueArr, setValueArr] = useState<number[]>([]);
+  const [index, setIndex] = useState<number>(defaultIndex);
 
+  // Set valueArr
   useEffect(() => {
-    if (props.onChange) props.onChange(value);
-  }, [value, props]);
+    const repeat = props.max - props.min + 1;
+    const tempArr: number[] = [];
+
+    for (let i = 0; i < repeat; i++) {
+      tempArr.push(i + props.min);
+    }
+    setValueArr(tempArr);
+  }, [props]);
+
+  // On change
+  useEffect(() => {
+    if (props.onChange) props.onChange(valueArr[index]);
+  }, [index, props, valueArr]);
 
   const upValue = () => {
-    let tempValue = value + step;
-    if (tempValue > props.max) {
-      tempValue = props.min + (tempValue - props.max) - 1;
+    let tempIndex = index + 1;
+    if (tempIndex > props.max) {
+      tempIndex = props.min;
     }
-    setValue(tempValue);
+    setIndex(tempIndex);
   };
 
   const downValue = () => {
-    let tempValue = value - step;
-    if (tempValue < props.min) {
-      tempValue = props.max + (tempValue - props.min) + 1;
+    let tempIndex = index - 1;
+    if (tempIndex < props.min) {
+      tempIndex = props.max;
     }
-    setValue(tempValue);
+    setIndex(tempIndex);
   };
 
   const scrollHandler = (e: React.WheelEvent) => {
@@ -48,15 +68,70 @@ export default function Slider(props: IProps) {
 
   return (
     <div className="slider">
-      <button onClick={upValue}>
-        <FontAwesomeIcon icon={faAngleUp} />
-      </button>
-      <div className="indicators" onWheel={scrollHandler}>
-        <p>{value}</p>
+      <div
+        className="indicators"
+        onWheel={scrollHandler}
+        style={{ height: `${HEIGHT * (COUNT * 2 - 1)}px` }}
+      >
+        <div className="current" style={{ top: `${HEIGHT * (COUNT - 1)}px`, height: HEIGHT }}></div>
+        {valueArr.map((element, i) => {
+          const style = getStyles(i, index, COUNT, HEIGHT, props.max);
+          style.height = `${HEIGHT}px`;
+          return (
+            <p className="element" key={i} style={style}>
+              {getDoubleDigit(element)}
+            </p>
+          );
+        })}
       </div>
-      <button onClick={downValue}>
-        <FontAwesomeIcon icon={faAngleDown} />
-      </button>
     </div>
   );
 }
+
+const getStyles = (
+  i: number,
+  index: number,
+  count: number,
+  height: number,
+  max: number
+): IStyles => {
+  const diff = i - index;
+  const rotateWeight = 80 / (count - 1);
+
+  if (Math.abs(diff) < count + 1) {
+    // no waiting area
+    return {
+      top: `${height * (count + diff - 1)}px`,
+      opacity: 1,
+      transform: `rotateX(${diff * rotateWeight}deg)`,
+    };
+  } else {
+    if (index < count && i > max - (count - index)) {
+      const diff = i - max - 1 - index;
+      return {
+        top: `${height * (count + diff - 1)}px`,
+        opacity: 1,
+        transform: `rotateX(${diff * rotateWeight}deg)`,
+      };
+    }
+    if (index > max - count && i < count) {
+      const diff = i + max - index;
+      return {
+        top: `${height * (count + diff)}px`,
+        opacity: 1,
+        transform: `rotateX(${diff * rotateWeight}deg)`,
+      };
+    }
+    // waiting area
+    return {
+      top: `${height * (count * 2)}px`,
+      opacity: 0,
+      transform: `rotateX(${diff * rotateWeight}deg)`,
+    };
+  }
+};
+
+const getDoubleDigit = (value: number) => {
+  if (value < 10) return `0${value}`;
+  return `${value}`;
+};
